252701e420d85409574b9f2bd55756e136c48089
unknown <abhijeet.badale@gmail.com>
2020-02-27 19:07:11 +0000

    first commit

---
+++ test.cmd
@@ -0,0 +1,7 @@
+cd C:\Users\Administrator\Downloads\apache-tomcat-9.0.31-windows-x64\apache-tomcat-9.0.31\bin
+set path=%path%;C:\Program Files\Java\jre1.8.0_241\bin
+set JRE_HOME=C:\Program Files\Java\jre1.8.0_241
+echo %JRE_HOME%
+pause
+startup.bat
+pause
